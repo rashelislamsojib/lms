a65a40d4f9a20da75739851c3172467d9f98d6ac
Rashel Islam Sojib <mdrashelislam86@gmail.com>
2024-05-19 17:09:54 +0600

    update
    update

---
+++ src/header.jsx
@@ -0,0 +1,10 @@
+import head  from "./assets/images/head.png"
+function Header() {
+  return (
+    <div className='parrent'>
+      <img src={head} alt="" />
+    </div>
+  )
+}
+
+export default Header
